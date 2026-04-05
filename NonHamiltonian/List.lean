3c965c853c1cfcdd8393199b23c8bdb3a97b93f9
Guilherme F. Lima <guilhermeflima@gmail.com>
2026-04-05 10:02:22 -0300

    NonHamiltonian: Update

--- NonHamiltonian/List.lean
+++ NonHamiltonian/List.lean
@@ -16,12 +16,49 @@ variable {α : Type u}
 
 namespace List
 
-/-- Computes all permutations of the given list. -/
+/-! ## connect -/
+
+/-- Constructs the list of pairs of consecutive elements in list. -/
+def connect : List α → List (α × α)
+  | [] => []
+  | a :: l =>
+    match l with
+    | [] => []
+    | a' :: _ => (a, a') :: connect l
+
+theorem connect_eq {l : List α} : l.connect = l.zip l.tail := by
+  induction l with simp_all [connect]
+  | cons _ l _ => cases l with simp
+
+theorem mem_connect_iff {l : List α} {p : α × α} :
+    p ∈ l.connect ↔ ∃ (i : Nat) (_ : i < l.length - 1),
+    l[i] = p.1 ∧ l[i+1] = p.2 := by
+  induction l with simp_all [connect]
+  | cons a l ih =>
+    constructor
+    · cases l with simp
+      | cons _ _ =>
+        rintro (_ | h)
+        · subst p; exists 0; simp_all
+        · rcases ih.mp h with ⟨j, hj, _⟩
+          simp at hj; exists j.succ; simp_all
+    · rintro ⟨i, hi, _, _⟩
+      cases l with
+      | nil => simp at hi
+      | cons _ _ =>
+        cases i with simp_all
+        | succ j =>
+          right; rw [← ih]; simp_all [connect]
+          exists j; simp at hi; simp_all
+
+/-! ## perms -/
+
+/-- Computes all permutations of a list. -/
 def perms : List α → List (List α)
   | [] => [[]]
   | a :: l =>
     (perms l).flatMap (λ p ↦
-      (List.finRange (p.length + 1)).map (λ i ↦ p.insertIdx i.val a))
+      (List.finRange (p.length + 1)).map (p.insertIdx · a))
 
 theorem mem_perms_self {l : List α} : l ∈ l.perms := by
   induction l with simp_all [perms]
